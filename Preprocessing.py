8604b2281953616d78c83f7c9d3174702ea8ef7f
Tikai7 <krimo-metal11@hotmail.com>
2023-01-29 19:08:52 +0100

    presentation version

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,17 +1,32 @@
 import cv2
+import numpy as np
 
 
 class Preprocessing:
 
     @staticmethod
     def process_both(im1, im2, DELTA, padding=False):
-        grayImg1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-        grayImg2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+        # grayImg1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        # grayImg2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+        yuvImg1 = cv2.cvtColor(im1, cv2.COLOR_RGB2YCrCb)
+        yuvImg2 = cv2.cvtColor(im2, cv2.COLOR_RGB2YCrCb)
+
+        luminance_image1 = np.expand_dims(yuvImg1[:, :, 0], axis=-1)
+        luminance_image2 = np.expand_dims(yuvImg2[:, :, 0], axis=-1)
+
+        luminance_image1 = luminance_image1.reshape(
+            (luminance_image1.shape[0], luminance_image1.shape[1]))
+        luminance_image2 = luminance_image2.reshape(
+            (luminance_image2.shape[0], luminance_image2.shape[1]))
+
+        # cv2.imshow("gray_image", luminance_image1)
+        # cv2.waitKey(3000)
 
         if padding:
-            grayImg1 = Preprocessing.add_padding(grayImg1, DELTA)
+            luminance_image1 = Preprocessing.add_padding(
+                luminance_image1, DELTA)
 
-        return grayImg1, grayImg2
+        return luminance_image1, luminance_image2
 
     @staticmethod
     def add_padding(image, DELTA=64):
